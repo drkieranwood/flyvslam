477724a34a6fdd3480d9c0bf6367e77fdd2359d4
Kiran Wood <kieranwood85@yahoo.co.uk>
2013-10-13 22:55:43 +0100

    Tidied up the waypoint class

--- include/flyvslam/waypoint_data.h
+++ include/flyvslam/waypoint_data.h
@@ -6,43 +6,52 @@
 
 
 //Class to store the waypoint data.
-//This class reads a list of waypoint data from file into a storage structure, then provides access to the waypoint data.
+//This class reads a list of waypoint data from file into a storage structure, 
+//then provides access to the waypoint data.
 class waypoint_data
 {
 	public:
 		//Constructor
 		waypoint_data();
 
-		//Get target position and yaw depending on the current time. The start_time must be set at the beginning of the flight.
-		//This function then determines the current target position and yaw depending upon the passed argument timeNow.
-		//The target yaw is updated after a call to getTargetPos(), hence always call getTargetYaw() immidiately after getTargetPos().
+		//Get target position and yaw depending on the current time.
+		//The start_time must be set at the beginning of the flight (or whenever 
+		//the waypoints should start.
+		//This function then determines the current target position and 
+		//yaw depending upon the passed argument timeNow (the current time).
+		//The target yaw is updated after a call to getTargetPos(), hence always 
+		//call getTargetYaw() immidiately after getTargetPos().
 		double getTargetYaw(void);
 		TooN::Vector<3, double> getTargetPos(ros::Time timeNow);
 
-		//Function to read all data from file. Assumes "waypoints.txt" is in the same dir.
-		//Ignores blank lines and lines starting with a hash (#). Returns true(1) if success or 0 elsewise.
+		//Function to read all data from file. Assumes "waypoints.txt" is 
+		//in the same dir. Ignores blank lines and lines starting with a 
+		//hash (#). Returns true(1) if success or 0 elsewise.
 		bool readWaypointData(void);
 
 
-
-		//Total number of waypoints read from file.
+		//Total number of waypoints read from the file.
 		int waypointCount;
 
 		//Pointer to a list of waypoint positions, look points, and durations
-		//This is the main storage structure. The actual arrays are created in the readWaypointData() function.
+		//This is the main storage structure. The actual correctly sized arrays 
+		//are created in the readWaypointData() function.
 		TooN::Vector<3, double> * waypointsPos;
 		TooN::Vector<3, double> * waypointsLook;
 		ros::Duration * waypointsTime;
 
-		//Current waypoint index (i.e. the last waypoint passed)
-		//The getTagetYaw() and getTargetPos() functions lerp between the current index and the next index.
+		//Current waypoint index (i.e. the last waypoint passed/reached)
+		//The getTagetYaw() and getTargetPos() functions lerp between the 
+		//current index and the next index over the specified duration time.
 		int currentIdx;
 
-		//Clock time the last waypoint changed, and start of the whole waypoint following flight.
+		//Clock time the last waypoint changed, and start of the whole 
+		//waypoint following flight.
 		ros::Time waypoint_time;
 		ros::Time start_time;
 
-		//Storage for the current lerp'd targetPos and targetLook. Also store the target yaw for checking of singularities.
+		//Storage for the current lerp'd targetPos and targetLook positions. 
+		//Also store the target yaw for checking of singularities.
 		TooN::Vector<3, double> targetPos;
 		double targetYaw;
 		TooN::Vector<3, double> targetLook;
--- src/waypoint_data.cpp
+++ src/waypoint_data.cpp
@@ -6,18 +6,22 @@
 
 
 //Constructor doesn't do much. 
-//The readWaypointData() function is used for most of the object initialization
+//The readWaypointData() function is used 
+//for most of this class's initialization.
 waypoint_data::waypoint_data()
 {
 	waypointCount=0;
 	currentIdx=0;
 	targetYaw=0.0;
+	targetPos=TooN::makeVector(0.0,0.0,0.0);
+	targetLook=TooN::makeVector(0.0,0.0,0.0);
 }
 
 
 
-//Uses the current stored targetPos and targetLook (which have been lerp'd inside the getTagetPos() function) to find 
-//the current target yaw angle. Returns the yaw angle in radians as a double. 
+//Uses the current stored targetPos and targetLook (which have been 
+//lerp'd inside the getTagetPos() function) to find the current target 
+//yaw angle. Returns the yaw angle in radians as a double. 
 //Requires getTargetPos() has been called recently.
 double waypoint_data::getTargetYaw(void)
 {
@@ -37,20 +41,23 @@ double waypoint_data::getTargetYaw(void)
 
 
 
-//Uses the current time, the start time and the current waypoint index to find the current target position and 
-//current target look position using linear interpolation.
-//Returns the current target position as a TooN vecor. Internally stores the current look position.
-//Requires that at least one call is made to this function between waypoints (i.e. the waypoint transition time 
-//cannot be smaller than the sampling interval)
+//Uses the current time, the start time and the current waypoint index 
+//to find the current target position and current target look position 
+//using linear interpolation. Returns the current target position as a 
+//TooN vecor. Internally stores the current look position. Requires that 
+//at least one call is made to this function between waypoints (i.e. the 
+//waypoint transition time cannot be smaller than the sampling interval)
 TooN::Vector<3, double> waypoint_data::getTargetPos(ros::Time timeNow)
 {
-	//Switch to next waypoint if the current waypoint transition time has been exceeded. 
+	//Switch to next waypoint if the current waypoint transition time 
+	//has been exceeded. 
 	if( (ros::Time::now().toSec()) > ((waypoint_time+waypointsTime[currentIdx]).toSec()) )
 	{	
-		//Set the transition time. NOTE: it is not based upon the time now, but rather the time when it should have changed.
+		//Set the transition time. NOTE: it is not based upon the 
+		//time now, but rather the time when it should have changed.
 		waypoint_time = waypoint_time + waypointsTime[currentIdx];
 
-		//Increment the index position and wrap if at the end
+		//Increment the index position and wrap if at the end.
 		currentIdx++;
 		if(currentIdx == waypointCount)
 		{
@@ -59,22 +66,25 @@ TooN::Vector<3, double> waypoint_data::getTargetPos(ros::Time timeNow)
 		ROS_INFO("flyvslam::Changed to waypoint: %i", (currentIdx+1) );
 	}
 
-
-	//Lerp between current index and next index, so find the next index value (wrap to first waypoint if at the end)
+	//Lerp between current index and next index, so find the next index 
+	//value (wrap to first waypoint if at the end)
 	int nextIdx = currentIdx+1;
 	if(nextIdx == waypointCount)
 	{
 		nextIdx = 0;
 	}
 
-	//Find the vector between the current waypoint and look-point and the next ones.
+	//Find the vector between the current waypoint and look-point 
+	//and the next ones.
 	TooN::Vector<3, double> tmpvecPos  = waypointsPos[nextIdx]  - waypointsPos[currentIdx];
 	TooN::Vector<3, double> tmpvecLook = waypointsLook[nextIdx] - waypointsLook[currentIdx];
 
-	//Find the proportion of time elepsed since the last waypoint change as a ratio with the transition time.
+	//Find the proportion of time elepsed since the last waypoint change 
+	//as a ratio with the transition time.
 	double lerpval = (double)((timeNow-waypoint_time).toSec()/waypointsTime[currentIdx].toSec());
 
-	//Find the position the MAV should be right now and the point it should be looking at
+	//Find the position the MAV should be right now and the point 
+	//it should be looking at
 	targetPos  = waypointsPos[currentIdx]  +(tmpvecPos *lerpval);
 	targetLook = waypointsLook[currentIdx] +(tmpvecLook*lerpval);
 
@@ -94,7 +104,8 @@ bool waypoint_data::readWaypointData(void)
 	TooN::Vector<3, double> tmp_waypointsLook[200];	
 	double tmp_waypointsTime[200];
 	
-	//This assumes the node is being run from the same directory as the "waypoints.txt" file.
+	//This assumes the node is being run from the same directory 
+	//as the "waypoints.txt" file.
 	//Open the file for reading.
 	std::string line;
 	std::ifstream myfile("waypoints.txt");
@@ -102,16 +113,18 @@ bool waypoint_data::readWaypointData(void)
 	if (myfile.is_open())
 	{
 		ROS_INFO("flyvslam::File opened...");
-		//Read waypoint info from file into tmp waypoints until end of file. Ignore lines starting with a # or $ or blank.
+		//Read waypoint info from file into tmp waypoints until 
+		//end of file. Ignore lines starting with a # or $ or blank.
 		while (myfile.good())
 		{			
-			getline (myfile,line);
-			if (line.find("$")==0)   			            //if a special commmand line starts with a $
+			getline (myfile,line);		
+			if (line.find("$")==0)                
 			{
-
+				//if a special commmand line starts with a $
 			}
-			else if((line!="")&&(line.find("#")!=0))		//else if not a comment (starts with a #) or blank
+			else if((line!="")&&(line.find("#")!=0))		
 			{
+				//else if not a comment (starts with a #) or blank
 				if(myfile.good())
 				{
 					//Create a cstring to hold the entire line
@@ -119,31 +132,36 @@ bool waypoint_data::readWaypointData(void)
 					cstr = new char [line.size()+1];
 
 					//Copy the line data into a c string (char array)
-    				strcpy (cstr, line.c_str());
+    					strcpy (cstr, line.c_str());
 
-					//Create a temp storage for the x,y,z pos x,y,z look and duration (7 in total)
-    				double * temp_read_values;
+					//Create a temp storage for the x,y,z pos x,y,z 
+					//look and duration (7 in total)
+    					double * temp_read_values;
 					temp_read_values = new double[7];
 
 					//Create a pointer to a position within the string.
 					char * pEnd;
 	
-					//Read the string as a double until the first white space. Then set pEnd as the position in the string reached.
+					//Read the string as a double until the first white 
+					//space. Then set pEnd as the position in the string reached.
 					//Store the value in the first position of the array.
 					temp_read_values[0] = (double)strtod(cstr,&pEnd);
 
-					//Moving from the previous point in the string, read the remining 6 values into the array.
+					//Moving from the previous point in the string, 
+					//read the remining 6 values into the array.
 					for(int ii=1 ; ii<7 ; ii++)
 					{
 						temp_read_values[ii] = (double)strtod(pEnd,&pEnd);
 					}
 
-					//Split the values into appropreate position, look, and time vectors.
+					//Split the values into appropreate position, look, 
+					//and time vectors.
 					tmp_waypointsPos[tmp_waypointCount] =TooN::makeVector(temp_read_values[0],temp_read_values[1],temp_read_values[2]);
 					tmp_waypointsLook[tmp_waypointCount]=TooN::makeVector(temp_read_values[3],temp_read_values[4],temp_read_values[5]);
 					tmp_waypointsTime[tmp_waypointCount]=temp_read_values[6];
 
-					//Once a yawpoint is added, increment the total waypoint counter
+					//Once a yawpoint is added, increment the total 
+					//waypoint counter
 					tmp_waypointCount++;
 				}
 			}
@@ -189,8 +207,8 @@ bool waypoint_data::readWaypointData(void)
 		waypointsLook[1] = TooN::makeVector(9999,0,-1);
 
 		waypointsTime = new ros::Duration[waypointCount];
-		waypointsTime[0]=ros::Duration(5.0);
-		waypointsTime[1]=ros::Duration(10.0);
+		waypointsTime[0]=ros::Duration(10.0);
+		waypointsTime[1]=ros::Duration(15.0);
 		
 		//Return -1 to indicate the file was not successfully read.
 		return -1;
