a6ec230fb6c2a6efb78847c3d110de6b71f239e3
Kieran Wood <kieranwood85@yahoo.co.uk>
2013-11-26 19:59:42 +0000

    Added lqg control to load matrices from file

--- include/flyvslam/lqg_control.h
+++ include/flyvslam/lqg_control.h
@@ -3,38 +3,47 @@
 
 //Required libraries and functions definitions
 #include <TooN/TooN.h>
+#include <string>
 
 
-//This is a class that runsa  sinple state space system. It accepts 
+//This is a class that runs a simple state-space system. It accepts 
 //updates from an input and modifies the state. It is for a discrete 
 //implemetation and the rate of update must be controlled externally.
 
-//1) Upon first creation the onject must have its [A B C D] matrices set 
+//1) Upon first creation the object must have its [A B C D] matrices set 
 //using setA() setB() ..... 
-//2) To update the system with an input use update() the returned vector 
+//2) To update the system with an input use update(), the returned vector 
 //is the output
-//3) The current output and state are also accessible using 
+//3) The current output and state are also accessible any time using 
 //getOutput(), and getState()
 
 //Note that the definition TooN::Matrix<> or TooN::Vector<> indicates a 
 //dynamically sized array.
 
+//Use TooN namespace to reduce the code visual complexity. Any Vector or 
+//Matrix objects are TooN versions. The Dynamic sizing is used so the 
+//number of states, inputs, and outputs are defined at run time.
 using namespace TooN;
+
 
 class lqg_control
 {
 	public:
-		//Constructor. No empty constructor available.
+		//Constructor with state-space sizes. No empty constructor available.
+		//states  -> Number of states
+		//inputs  -> Number of inputs
+		//outputs -> Number of outputs
 		lqg_control(int states,int inputs,int outputs);
 		
 		//Update the control by one iteration. It is up to the caller to 
-		//ensure the rate is appropreate.
+		//ensure the rate is appropriate.
+		//inputVec -> The new/current state-space input vector.
 		Vector<Dynamic,double> update(Vector<Dynamic,double> inputVec);
 		
-		//Returns the current internal state
+		//Returns the current internal state.
 		Vector<Dynamic,double> getState(void);
 		
-		//Returns the current output
+		//Returns the current output.
 		Vector<Dynamic,double> getOutput(void);
 		
 		//Functions to set the state-space matrices
@@ -47,27 +56,46 @@ class lqg_control
 		Matrix<Dynamic,Dynamic,double> getA(void);
 		Matrix<Dynamic,Dynamic,double> getB(void);
 		Matrix<Dynamic,Dynamic,double> getC(void);
-		Matrix<Dynamic,Dynamic,double> getD(void);	
+		Matrix<Dynamic,Dynamic,double> getD(void);
 		
 		
+		//Functions to get sizes and rate
+		int getNumStates(void);
+		int getNumInputs(void);
+		int getNumOutputs(void);
+		int getRate(void);
 		
-		//Storage for the system matrices
+		
+		double ctrlRate;
+		
+		
+	private:
+		//Storage for the system matrices. They are pointers because the
+		//size is unknown until run-time when they are initialised.
 		Matrix<Dynamic,Dynamic,double>* matA;
 		Matrix<Dynamic,Dynamic,double>* matB;
 		Matrix<Dynamic,Dynamic,double>* matC;
 		Matrix<Dynamic,Dynamic,double>* matD;
 			
-		//The internal state storage and last input and output
+		//The internal state storage and last input and output.
 		Vector<Dynamic,double>* currentState;
 		Vector<Dynamic,double>* currentInput;
 		Vector<Dynamic,double>* currentOutput;
 		
 		//Store the number of states, inputs and outputs.
-		int statesCount;
-		int inputsCount;
-		int outputsCount;
-
+		int staCount;
+		int inpCount;
+		int outCount;
 };
+
+
+
+//Function to load a set of control gains from the specified file into 
+//a lqg_control object. This function has an accompanying Matlab script
+//which creates the gain files. When one is updated the other sould be 
+//also. This function returns a pointer to a new object created within 
+//the function.
+lqg_control * loadControlGainsFromFile(std::string filename);
 
 
 #endif
--- src/lqg_control.cpp
+++ src/lqg_control.cpp
@@ -1,17 +1,29 @@
 #include <flyvslam/lqg_control.h>
 
-using namespace TooN;
+//Required libraries and functions definitions
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "ros/ros.h"
 
+
+//Use TooN namespace to reduce the code visual complexity. Any Vector or 
+//Matrix objects are TooN versions. The Dynamic sizing is used so the 
+//number of states, inputs, and outputs are defined at run time.
+using namespace TooN;
+#include "ros/ros.h"
+
+//Constructor
 lqg_control::lqg_control(int states,int inputs,int outputs)
 {
-	//The default system is a SISO pass-through. i.e the system has no
-	//dynamics and outputs the input immidiately.
-	//The class defines a set of pointers to dynamic matrices.
-	//These are now initialised with an empty 1x1 matirx.
-	statesCount  = states;
-	inputsCount  = inputs;
-	outputsCount = outputs;
+	//Store the state-space sizes.
+	this->staCount  = states;
+	this->inpCount  = inputs;
+	this->outCount  = outputs;
 	
+	//Initialise the matrices to be the appropriate sizes as per the 
+	//given arguments. The {} brackets are used to ensure the automatic
+	//destruction of temporary variables.
 	{
 		Matrix<Dynamic,Dynamic,double> tempMat(states,states);
 		matA = new Matrix<Dynamic,Dynamic,double>(tempMat);
@@ -29,24 +41,29 @@ lqg_control::lqg_control(int states,int inputs,int outputs)
 		matD = new Matrix<Dynamic,Dynamic,double>(tempMat);
 	}
 	
-	//States inputs and outputs
+	
+	//Initialise the state, input, and output storage vectors to be the 
+	//appropriate sizes as per the given arguments.
 	{
-		Vector<Dynamic,double> tempVec(statesCount);
+		Vector<Dynamic,double> tempVec(states);
 		currentState = new Vector<Dynamic,double>(tempVec);
 		*currentState = Zeros;
 	}
 	{
-		Vector<Dynamic,double> tempVec(inputsCount);
+		Vector<Dynamic,double> tempVec(inputs);
 		currentInput = new Vector<Dynamic,double>(tempVec);
 		*currentInput = Zeros;
 	}
 	{
-		Vector<Dynamic,double> tempVec(outputsCount);
+		Vector<Dynamic,double> tempVec(outputs);
 		currentOutput = new Vector<Dynamic,double>(tempVec);
 		*currentOutput = Zeros;
 	}
-}
+	
+} //~Constructor
 
+
+// Update
 Vector<Dynamic,double> lqg_control::update(Vector<Dynamic,double> inputVec)
 {
 	//Create the output
@@ -55,7 +72,7 @@ Vector<Dynamic,double> lqg_control::update(Vector<Dynamic,double> inputVec)
 	
 	//Update the state
 	//x[k+1] = A*x[k] + B*u[k]
-	*currentState = ((*matA)*(*currentState)) + ((*matB)*(inputVec));;
+	*currentState = ((*matA)*(*currentState)) + ((*matB)*(inputVec));
 	
 	//Update the input storage
 	*currentInput = inputVec;
@@ -63,16 +80,19 @@ Vector<Dynamic,double> lqg_control::update(Vector<Dynamic,double> inputVec)
 	//Return the output. De-reference the pointer so it is returned as 
 	//a dynamic Vector object.
 	return *currentOutput;
-}
+	
+} //~update
 
-//Returns the current state
+
+//Returns the current state.
 Vector<Dynamic,double> lqg_control::getState(void)
 {
 	//Return the output. De-reference the pointer so it is returned as 
 	//a dynamic Vector object.
 	return *currentState;
 }
-		
+
+
 //Returns the current output
 Vector<Dynamic,double> lqg_control::getOutput(void)
 {
@@ -81,7 +101,9 @@ Vector<Dynamic,double> lqg_control::getOutput(void)
 	return *currentOutput;	
 }
 
-//Functions to setup the state space matrices
+
+//Functions to setup the state space matrices. The arguments must be 
+//TooN dynamic matrix objects passed by value.
 void lqg_control::setA(Matrix<Dynamic,Dynamic,double> inMat)
 {
 	*matA = inMat;	
@@ -100,7 +122,8 @@ void lqg_control::setD(Matrix<Dynamic,Dynamic,double> inMat)
 }
 
 
-//Functions to retrieve the state space matrices
+//Functions to retrieve the state space matrices as TooN dynamic matrix 
+//objects. The pointers are de-referenced to return by value.
 Matrix<Dynamic,Dynamic,double> lqg_control::getA(void)
 {
 	return *matA;
@@ -116,6 +139,139 @@ Matrix<Dynamic,Dynamic,double> lqg_control::getC(void)
 Matrix<Dynamic,Dynamic,double> lqg_control::getD(void)
 {
 	return *matD;
+}
+
+//Functions to get sizes and rate
+int lqg_control::getNumStates(void)
+{
+	return staCount;
+}
+int lqg_control::getNumInputs(void)
+{
+	return inpCount;
+}
+int lqg_control::getNumOutputs(void)
+{
+	return outCount;
+}
+int lqg_control::getRate(void)
+{
+	return ctrlRate;
+}
+
+
+//Function to create a new object and load the matrix values from file.
+//All values are also written to the rosout stream. This is a little
+//cluttered but ensures each flight logs the controller used.
+lqg_control * loadControlGainsFromFile(std::string filename)
+{
+	//Temp string to hold each line of text from the file.
+	std::string line;
+	
+	//Attempt to open the file for reading.
+	std::ifstream myfile(filename.c_str());
+
+	if (myfile.is_open())
+	{
+		//If the file opened for reading.
+		ROS_INFO("flyvslam::Gain file opened...");
+		
+		//Get the first four lines relating to the matrix sizes and
+		//implementation rate. 
+		int numSta = 0;
+		int numInp = 0;
+		int numOut = 0;
+		double rate = 0.0;
+		myfile >> numSta >> numInp >> numOut >> rate;
+		ROS_INFO("flyvslam::[Sta,Inp,Out,Rate] = [%d,%d,%d,%10.5f]",numSta,numInp,numOut,rate);
+		
+		//Use the sizes to initialise a new lqg_control object
+		lqg_control * newCtrl = new lqg_control(numSta,numInp,numOut);
+		newCtrl->ctrlRate=rate;
+		
+		//Iterate through the rest of the file to fill in the matrix
+		//values. Fist read them to a temp variable then copy into 
+		//lqg_control object.
+		
+		//A A A A
+		{
+			Matrix<Dynamic,Dynamic,double> tempMat(numSta,numSta);
+			for (int ii=0;ii<numSta;ii++)
+			{
+				for (int jj=0;jj<numSta;jj++)
+				{
+					double temp = 0.0;
+					myfile >> temp;
+					tempMat(ii,jj) = temp;
+					ROS_INFO("flyvslam::A(%d,%d) = (%10.5f)",ii,jj,temp);
+				}
+			}
+			newCtrl->setA(tempMat);
+		}
+		
+		//B B B B
+		{
+			Matrix<Dynamic,Dynamic,double> tempMat(numSta,numInp);
+			for (int ii=0;ii<numSta;ii++)
+			{
+				for (int jj=0;jj<numInp;jj++)
+				{
+					double temp = 0.0;
+					myfile >> temp;
+					tempMat(ii,jj) = temp;
+					ROS_INFO("flyvslam::B(%d,%d) = (%10.5f)",ii,jj,temp);
+				}
+			}
+			newCtrl->setB(tempMat);
+		}
+		
+		//C C C C
+		{
+			Matrix<Dynamic,Dynamic,double> tempMat(numOut,numSta);
+			for (int ii=0;ii<numOut;ii++)
+			{
+				for (int jj=0;jj<numSta;jj++)
+				{
+					double temp = 0.0;
+					myfile >> temp;
+					tempMat(ii,jj) = temp;
+					ROS_INFO("flyvslam::C(%d,%d) = (%10.5f)",ii,jj,temp);
+				}
+			}
+			newCtrl->setC(tempMat);
+		}
+		
+		//D D D D
+		{
+			Matrix<Dynamic,Dynamic,double> tempMat(numOut,numInp);
+			for (int ii=0;ii<numOut;ii++)
+			{
+				for (int jj=0;jj<numInp;jj++)
+				{
+					double temp = 0.0;
+					myfile >> temp;
+					tempMat(ii,jj) = temp;
+					ROS_INFO("flyvslam::D(%d,%d) = (%10.5f)",ii,jj,temp);
+				}
+			}
+			newCtrl->setD(tempMat);
+		}
+		
+		
+		//Close the file.
+		myfile.close();
+		ROS_INFO("flyvslam::Gain file closed.");
+		
+		//Return the pointer to the newly created and populated object.
+		return newCtrl;
+	}
+	else
+	{
+		//If the file is not opened, then do not create anything and 
+		//return a null pointer.
+		ROS_INFO("flyvslam:: Could not open control gain file.");
+		return NULL;
+	}
 }
 
 //eof
